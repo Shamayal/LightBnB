ef744e23c9dd5fcca0b236e8f162822f7d474dd2
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-04 21:17:54 -0400

    refactored getUserWithId

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -32,7 +32,10 @@ const pool = new Pool({
 
 const getUserWithEmail = (email) => {
   return pool
-    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .query(`
+    SELECT * 
+    FROM users 
+    WHERE email = $1`, [email])
     .then((result) => {
       const users = result.rows;
       //console.log("check users:", result)
@@ -52,8 +55,28 @@ const getUserWithEmail = (email) => {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// };
+
+const getUserWithId = (id) => {
+  return pool
+    .query(`
+    SELECT * 
+    FROM users 
+    WHERE id = $1`, [id])
+    .then((result) => {
+      const users = result.rows;
+      console.log("check users:", result)
+      if (users.length === 0) {
+        return null;
+      } else {
+        return result.rows[0];
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -67,6 +90,7 @@ const addUser = function (user) {
   users[userId] = user;
   return Promise.resolve(user);
 };
+
 
 /// Reservations
 
@@ -97,7 +121,10 @@ const getAllReservations = function (guest_id, limit = 10) {
 
 const getAllProperties = (options, limit = 10) => {
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(`
+    SELECT * 
+    FROM properties 
+    LIMIT $1`, [limit])
     .then((result) => {
       //console.log(result.rows);
       return result.rows;
